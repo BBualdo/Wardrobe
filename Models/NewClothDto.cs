140fcd893660faa6ada08a72982e79028b6f6938
Sebastian <mrseb10@gmail.com>
2024-06-24 22:17:26 +0200

    Form Validation

--- Models/NewClothDto.cs
+++ Models/NewClothDto.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Wardrobe.BBualdo.Models;
 
 public class NewClothDto
 {
+    [Required]
+    [MinLength(3, ErrorMessage = "Name must be at least 3 characters long.")]
     public required string Name { get; set; }
+
     public required string Category { get; set; }
     public required string Size { get; set; }
+
+    [Required]
+    [MinLength(4, ErrorMessage = "Name must be at least 4 characters long.")]
+    [StringLength(20, ErrorMessage = "Name can't be longer than 20 characters.")]
     public required string Color { get; set; }
-    public required string Brand { get; set; }
+
+    [Required] public required string Brand { get; set; }
     public string? ImagePath { get; set; }
 }
